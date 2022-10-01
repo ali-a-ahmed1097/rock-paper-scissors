8932c2728779b611046a19c227c3688542deba9e
Ali <ali-a-ahmed1097@hotmail.com>
2022-10-01 18:20:58 -0400

    Finished the project with the necessary specifications for now. TODO (in the future):     Add UI

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # rock-paper-scissors
 
-This is a simple game of rock, paper, scissors. Right now, it is played using the console so the HTML page is basically just blank.
+This is a simple game of rock, paper, scissors. Right now, it is played using the console so the HTML page is basically just blank. In the future, I intend to add a UI.
---
+++ rps.js
@@ -0,0 +1,60 @@
+const ROCK = 1;
+const PAPER = 2;
+const SCISSORS = 3;
+const computerWins = 0;
+const playerWins = 1;
+
+function convertToRPS(rps) {
+    if (rps === ROCK) return 'Rock';
+    else if (rps === PAPER) return 'Paper';
+    else return 'Scissors';
+}
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function getPlayerChoice() {
+    while (true) {
+        let choice = prompt('Rock, Paper, or Scissors?');
+        choice = choice.toLowerCase();
+        console.log(choice);
+        if (choice === 'rock') return ROCK;
+        else if (choice === 'paper') return PAPER;
+        else if (choice === 'scissors') return SCISSORS;
+    }
+}
+
+function computeChoiceOutcome(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) return -1;
+    else if ((playerChoice === ROCK && computerChoice === PAPER)
+        || (playerChoice === SCISSORS && computerChoice === ROCK)
+        || (playerChoice === PAPER && computerChoice === SCISSORS)) 
+            return computerWins;
+    else return playerWins;
+}
+
+function game() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    while (computerPoints < 5 && playerPoints < 5)
+    {
+        let pc = getPlayerChoice();
+        let cc = getComputerChoice();
+        let outcome = computeChoiceOutcome(pc, cc);
+        if (outcome === computerWins) {
+            console.log(`You lose! ${convertToRPS(cc)} beats ${convertToRPS(pc)}`);
+            computerPoints++;
+        }
+        else if (outcome === playerWins)
+        { 
+            console.log(`You win! ${convertToRPS(pc)} beats ${convertToRPS(cc)}`);
+            playerPoints++;
+        }
+        else console.log(`Tie! Both chose ${convertToRPS(cc)}`);
+
+        console.log(`Player points: ${playerPoints}, Computer points: ${computerPoints}`);
+    }
+    if (computerPoints === 5) console.log('Computer wins. You lose!');
+    else console.log('Congratulations! You win!');
+}
